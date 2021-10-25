b01eb8c55d8bcdf281b4c210ed5a7b2146d2a8ab
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-10-25 22:42:53 +0600

    Finsihed Squre Component

--- src/components/sub-components/Square.jsx
+++ src/components/sub-components/Square.jsx
@@ -3,6 +3,7 @@ import React from "react";
 const Square = props => {
     return (
         <button
+            className='board-btn'
             onClick={props.sqrHandleClick}
         >
             {props.sqrValue}
