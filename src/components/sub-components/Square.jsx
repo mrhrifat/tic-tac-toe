49e689891a6c9c3486188eb1648aaa5499e62f5c
Rakibul Hassan Rifat <mrhrifat383759@gmail.com>
2021-11-12 00:03:14 +0600

    Remove SASS & Add Inline CSS

--- src/components/sub-components/Square.jsx
+++ src/components/sub-components/Square.jsx
@@ -5,9 +5,16 @@ const Square = props => {
         <button
             className='board-btn'
             onClick={props.sqrHandleClick}
+            style={{
+                width: '50px',
+                height: '50px',
+                padding: '50px',
+                border: '1px solid #2d3e50',
+                backgroundColor: '#34495e',
+            }}
         >
             {props.sqrValue}
-        </button>
+        </button >
     )
 }
 export default Square
